4f557e52b2dc39d543bdf763299f7a9f20309293
Srushti Myakal <srushtimyakal99@gmail.com>
2026-09-03 14:00:18 +0530

    Add gree() function

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+void greet(const char *name) {
+    printf("Hello, %s!Welcome to your GitHub portfolio.\n",name);
+}
+
 int main()
 {
     printf("hello world!");
+    greet("Ada");
     return 0;
 }
